cb2383c74077fa75ea776a0c7aba105d41dfb260
lcagustini <lucas.c.agustini@gmail.com>
2021-04-10 14:04:34 -0300

    Refactored code and allowed for different branchs to watch

--- src/main.rs
+++ src/main.rs
@@ -6,57 +6,69 @@ use std::thread;
 use std::env;
 use dotenv;
 
-#[tokio::main]
-async fn main() {
-    let _ = dotenv::dotenv();
-    let token = env::var("DISCORD_TOKEN").expect("token");
+pub struct Watcher {
+    pub branch: String,
+    pub repo: Repository,
+}
+impl Watcher {
+    pub fn new(path: &str, branch: String) -> Watcher {
+        let repo = match Repository::open(path) {
+            Ok(repo) => repo,
+            Err(e) => panic!("failed to open: {}", e),
+        };
+        return Watcher { branch: branch, repo: repo };
+    }
 
-    let client = Http::new_with_token(&token);
+    fn fast_forward(&self) -> Result<(), git2::Error> {
+        let mut callbacks = git2::RemoteCallbacks::new();
+        callbacks.credentials(|_, _, _| {
+            let user = env::var("GIT_USER").expect("user");
+            let pass = env::var("GIT_PASSWORD").expect("pass");
+            return git2::Cred::userpass_plaintext(&user, &pass);
+        });
 
-    let repo = match Repository::open("/home/lucas/Documents/test/") {
-        Ok(repo) => repo,
-        Err(e) => panic!("failed to open: {}", e),
-    };
+        let mut opts = git2::FetchOptions::new();
+        opts.remote_callbacks(callbacks);
 
-    loop {
-        thread::sleep(Duration::new(10, 0));
+        let _ = self.repo.find_remote("origin")?.fetch(&[&self.branch], Some(&mut opts), None)?;
 
-        let id = repo.head().unwrap().peel_to_commit().unwrap().id();
-        let _ = fast_forward(&repo).map_err(|e| println!("{:?}", e));
-        let new_id = repo.head().unwrap().peel_to_commit().unwrap().id();
+        let fetch_head = self.repo.find_reference("FETCH_HEAD")?;
+        let fetch_commit = self.repo.reference_to_annotated_commit(&fetch_head)?;
+        let analysis = self.repo.merge_analysis(&[&fetch_commit])?;
+        if analysis.0.is_up_to_date() {
+            Ok(())
+        } else if analysis.0.is_fast_forward() {
+            let refname = format!("refs/heads/{}", self.branch);
+            let mut reference = self.repo.find_reference(&refname)?;
+            reference.set_target(fetch_commit.id(), "Fast-Forward")?;
+            self.repo.set_head(&refname)?;
+            self.repo.checkout_head(Some(git2::build::CheckoutBuilder::default().force()))
+        } else {
+            Err(git2::Error::from_str("Fast-forward only!"))
+        }
+    }
 
-        if id != new_id {
-            let _ = ChannelId::from(829837845916549165).say(&client, format!("Someone pushed to develop! Now at {}", new_id)).await;
+    pub async fn watch(self) {
+        let token = env::var("DISCORD_TOKEN").expect("token");
+        let client = Http::new_with_token(&token);
+        loop {
+            thread::sleep(Duration::new(10, 0));
+
+            let id = self.repo.head().unwrap().peel_to_commit().unwrap().id();
+            let _ = self.fast_forward().map_err(|e| println!("{:?}", e));
+            let new_id = self.repo.head().unwrap().peel_to_commit().unwrap().id();
+
+            if id != new_id {
+                let _ = ChannelId::from(829837845916549165).say(&client, format!("Someone pushed to develop! Now at {}", new_id)).await;
+            }
         }
     }
 }
 
-fn fast_forward(repo: &Repository) -> Result<(), git2::Error> {
-    let mut callbacks = git2::RemoteCallbacks::new();
-    callbacks.credentials(|_, _, _| {
-        let user = env::var("GIT_USER").expect("user");
-        let pass = env::var("GIT_PASSWORD").expect("pass");
-        return git2::Cred::userpass_plaintext(&user, &pass);
-    });
+#[tokio::main]
+async fn main() {
+    let _ = dotenv::dotenv();
 
-    let mut opts = git2::FetchOptions::new();
-    opts.remote_callbacks(callbacks);
-
-    let _ = repo.find_remote("origin")?.fetch(&["master"], Some(&mut opts), None)?;
-
-    let fetch_head = repo.find_reference("FETCH_HEAD")?;
-    let fetch_commit = repo.reference_to_annotated_commit(&fetch_head)?;
-    let analysis = repo.merge_analysis(&[&fetch_commit])?;
-    if analysis.0.is_up_to_date() {
-        Ok(())
-    } else if analysis.0.is_fast_forward() {
-        let refname = format!("refs/heads/{}", "master");
-        let mut reference = repo.find_reference(&refname)?;
-        reference.set_target(fetch_commit.id(), "Fast-Forward")?;
-        repo.set_head(&refname)?;
-        repo.checkout_head(Some(git2::build::CheckoutBuilder::default().force()))
-    } else {
-        Err(git2::Error::from_str("Fast-forward only!"))
-    }
+    let watcher = Watcher::new("/home/lucas/Documents/test", "master".to_string());
+    watcher.watch().await;
 }
-
